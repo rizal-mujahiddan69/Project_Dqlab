84b4801f9b47141aad16fb6e1ce2f53c1ef0cd39
rizal-mujahiddan69 <ganteng3rizal@apps.ipb.ac.id>
2022-03-18 07:59:27 +0700

    cek

--- server.R
+++ server.R
@@ -4,18 +4,6 @@ library(readxl)
 library(tidyverse)
 library(visdat)
 
-data_src <- read_excel("Data_World.xls",sheet = "Data",skip = 2)
-names(data_src)[1:4] <- make.names(names(data_src)[1:4])
-names(data_src)
-
-data_src <- data_src %>% 
-  select(-c(Country.Name,Country.Code)) %>% 
-  pivot_longer(names(data_src)[-c(1:4)],names_to = "year")
-
-data_src$year <-  as.numeric(data_src$year)
-
-
-
 server <- function(input, output,session) {
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
@@ -26,7 +14,7 @@ server <- function(input, output,session) {
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
   })
   output$Plotting <- renderPlot({
-    plot(data_src$year,data_src$value)
+    plot(c(2,3,4),c(2,3,4))
   })
   session$allowReconnect(TRUE)
 }
